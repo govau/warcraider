cffa840d0797fb4c1dac9503744928e62f700796
Alex Sadleir <maxious@lambdacomplex.org>
2019-09-01 22:16:23 +1000

    refactor out blacklist

---
+++ src/blacklist.rs
@@ -0,0 +1,53 @@
+pub fn on_blacklist(hostname: &str, url: &str) -> bool {
+    let hostname_blacklist = [
+        "insolvencynotices.asic.gov.au",
+        "data.gov.au",
+        "trove.nla.gov.au",
+        "data.aad.gov.au",
+        "www.trove.nla.gov.au",
+        "epubs.aims.gov.au",
+        "services.aad.gov.au",
+        "results.aec.gov.au",
+        "periodicdisclosures.aec.gov.au",
+        "transcribe.naa.gov.au",
+        "bookshop.nla.gov.au",
+        "recordsearch.naa.gov.au",
+        "library.nma.gov.au",
+        "abr.business.gov.au",
+        "collections.anmm.gov.au",
+        "elibrary.gbrmpa.gov.au",
+        "channelfinder.acma.gov.au",
+        "vrroom.naa.gov.au",
+        "www.tenders.gov.au",
+        "dmzapp17p.ris.environment.gov.au",
+        "discoveringanzacs.naa.gov.au",
+        "elibrary.gbrmpa.gov.au",
+        "neats.nopta.gov.au",
+        "results.aec.gov.au",
+        "recordsearch.naa.gov.au",
+        "services.aad.gov.au",
+        "soda.naa.gov.au",
+        "stat.data.abs.gov.au",
+        "store.anmm.gov.au",
+        "toiletmap.gov.au",
+        "training.gov.au",
+        "transcribe.naa.gov.au",
+        "wels.agriculture.gov.au",
+        "www.padil.gov.au",
+        "www.screenaustralia.gov.au",
+        "edit.acnc.gov.au",
+    ];
+    let url_blacklist = ["http://www.nepc.gov.au/system/files/resources/45fee0f3-1266-a944-91d7-3b98439de8f8/files/dve-prepwk-project2-1-diesel-complex-cuedc.xls" ,
+                     "https://www.ncver.edu.au/__data/assets/word_doc/0013/3046/2221s.doc" ,
+                     "https://www.acma.gov.au/-/media/Broadcast-Carriage-Policy/Information/Word-document/reg_qld-planning_data-docx.docx?la=en",
+                    "https://www.acma.gov.au/-/media/Broadcasting-Spectrum-Planning/Information/Word-Document-Digital-TV/Planning-data-Regional-Queensland-TV1.docx?la=en" ,
+                    "https://beta.dva.gov.au/sites/default/files/files/providers/vendor/medvendor1sept2015.xls" ,
+                     "https://www.ppsr.gov.au/sites/g/files/net3626/f/B2G%20Interface%20Specification%20R4.doc" ,
+                     "http://guides.dss.gov.au/sites/default/files/2003_ABSTUDY_Policy_Manual.docx",
+                     "http://www.nepc.gov.au/system/files/resources/45fee0f3-1266-a944-91d7-3b98439de8f8/files/dve-prepwk-project2-1-diesel-complex-simp-cuedc.xls"];
+    return hostname_blacklist.contains(&hostname)
+        || url_blacklist.contains(&url)
+        || url.matches("ca91-4-xd").count() > 0
+        || url.matches("sbs.com.au/ondemand").count() > 0
+        || url.matches("sbs.com.au/news").count() > 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ use std::io;
 use std::io::Read;
 use std::str::FromStr;
 use warcraider::*;
+mod blacklist;
 
 use addr::DomainName;
 use avro_rs::{to_value, types::Record, Schema, Writer};
@@ -221,7 +222,10 @@ fn process_warc(
         warc.next();
         loop {
             if i >= finish_at {
-                info!("no more warc records in {} between {} and {}", warc_filename, start_at, finish_at);
+                info!(
+                    "no more warc records in {} between {} and {}",
+                    warc_filename, start_at, finish_at
+                );
                 break;
             } else if i <= start_at {
                 //debug!("skipping {}",i);
@@ -236,12 +240,14 @@ fn process_warc(
                     warc_result!(warc),
                     warc_result!(warc),
                     warc_result!(warc),
-                    warc_result!(warc)
+                    warc_result!(warc),
                 ]
                 .par_iter()
                 .filter_map(move |item| {
                     let warc_record = item;
-                    if warc_record.version != "0" && warc_record.header.get(&"WARC-Type".into()) == Some(&"response".into()) {
+                    if warc_record.version != "0"
+                        && warc_record.header.get(&"WARC-Type".into()) == Some(&"response".into())
+                    {
                         let url = String::from("")
                             + warc_record
                                 .header
@@ -254,61 +260,16 @@ fn process_warc(
                             .unwrap_or(&String::from("0"))
                             .parse::<i32>()
                             .unwrap();
-                    let hostname = match HOSTNAME_REGEX.captures(&url) {
-                        Some(caps) => String::from(caps.get(1).unwrap().as_str()),
-                        None => String::from(""),
-                    };
-                         if [ "insolvencynotices.asic.gov.au",
-                                "data.gov.au",
-                                "trove.nla.gov.au",
-                                "data.aad.gov.au",
-                                "www.trove.nla.gov.au",
-                                "epubs.aims.gov.au",
-                                "services.aad.gov.au",
-                                "results.aec.gov.au",
-                                "periodicdisclosures.aec.gov.au",
-                                "transcribe.naa.gov.au",
-                                "bookshop.nla.gov.au",
-                                "recordsearch.naa.gov.au",
-                                "library.nma.gov.au",
-                                "abr.business.gov.au",
-                                "collections.anmm.gov.au",
-                                "elibrary.gbrmpa.gov.au",
-                                "channelfinder.acma.gov.au",
-                                "vrroom.naa.gov.au",
-                                "www.tenders.gov.au",
-                                "dmzapp17p.ris.environment.gov.au",
-                                "discoveringanzacs.naa.gov.au",
-                                "elibrary.gbrmpa.gov.au",
-                                "neats.nopta.gov.au",
-                                "results.aec.gov.au",
-                                "recordsearch.naa.gov.au",
-                                "services.aad.gov.au",
-                                "soda.naa.gov.au",
-                                "stat.data.abs.gov.au",
-                                "store.anmm.gov.au",
-                                "toiletmap.gov.au",
-                                "training.gov.au",
-                                "transcribe.naa.gov.au",
-                                "wels.agriculture.gov.au",
-                                "www.padil.gov.au",
-                                "www.screenaustralia.gov.au",
-                                "edit.acnc.gov.au"
-                    ].contains(&hostname.as_str()) ||
-                    ["http://www.nepc.gov.au/system/files/resources/45fee0f3-1266-a944-91d7-3b98439de8f8/files/dve-prepwk-project2-1-diesel-complex-cuedc.xls" ,
-                     "https://www.ncver.edu.au/__data/assets/word_doc/0013/3046/2221s.doc" ,
-                     "https://www.acma.gov.au/-/media/Broadcast-Carriage-Policy/Information/Word-document/reg_qld-planning_data-docx.docx?la=en",
-                    "https://www.acma.gov.au/-/media/Broadcasting-Spectrum-Planning/Information/Word-Document-Digital-TV/Planning-data-Regional-Queensland-TV1.docx?la=en" ,
-                    "https://beta.dva.gov.au/sites/default/files/files/providers/vendor/medvendor1sept2015.xls" ,
-                     "https://www.ppsr.gov.au/sites/g/files/net3626/f/B2G%20Interface%20Specification%20R4.doc" ,
-                     "http://guides.dss.gov.au/sites/default/files/2003_ABSTUDY_Policy_Manual.docx",
-                     "http://www.nepc.gov.au/system/files/resources/45fee0f3-1266-a944-91d7-3b98439de8f8/files/dve-prepwk-project2-1-diesel-complex-simp-cuedc.xls"].contains(&url.as_str()) ||
-                    url.matches("ca91-4-xd").count() > 0 ||
-                     url.matches("sbs.com.au/ondemand").count() > 0 ||
-                     url.matches("sbs.com.au/news").count() > 0
-                    {
-                        debug!("{}:{} excluded url {} as too complex or too large", warc_number, i, url);
-                        None
+                        let hostname = match HOSTNAME_REGEX.captures(&url) {
+                            Some(caps) => String::from(caps.get(1).unwrap().as_str()),
+                            None => String::from(""),
+                        };
+                        if blacklist::on_blacklist(&hostname, &url) {
+                            debug!(
+                                "{}:{} excluded url {} as too complex or too large",
+                                warc_number, i, url
+                            );
+                            None
                         } else {
                             Some(WarcResult {
                                 url,
